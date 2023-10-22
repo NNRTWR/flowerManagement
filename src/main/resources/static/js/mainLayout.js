1c5e20a207bc6fbc2baf8a1fe7a2c4769dd6bb8f
Pecthgamon Sangnual <petchgamon.s@ku.th>
2023-10-22 22:22:17 +0700

    order

--- src/main/resources/static/js/mainLayout.js
+++ src/main/resources/static/js/mainLayout.js
@@ -1,24 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     const drawerToggle = document.getElementById('my-drawer');
     const content = document.getElementById('content-layout');
+    const nav = document.getElementById('nav');
+    const side = document.getElementById('side');
 
     //default
     content.style.width = 'calc(100% - 256px)';
     content.style.marginLeft = '256px';
-    // drawerToggle.style.zIndex = '0'
-    // content.style.zIndex = '10';
+    //drawerToggle.style.zIndex = '20'
+    content.style.zIndex = '10';
+    nav.style.zIndex = '20'
+    side.style.zIndex = '10'
 
     drawerToggle.addEventListener('change', function() {
         if (drawerToggle.checked) {
             //sidebar เปิด
             content.style.width = 'calc(100% - 256px)'; // 64px เป็นขนาดของ sidebar
             content.style.marginLeft = '256px';
-            // content.style.zIndex = '0';
         } else {
             // Sidebar ปิด
             content.style.width = '100%';
             content.style.marginLeft = '0';
-            // content.style.zIndex = '10';
+            
         }
     });
 });
